ccab587a4d95de81766d71beb0c07741ac1914b5
Dan Coughlin <dan.coughlin@gmail.com>
2016-03-12 13:55:17 -0500

    this is the complete step 2 and includes the TODO for step 3. This updates the label when Find Seats button is clicked and has the method signatures in for step 3.

--- AirplaneSeatingViewer/src/airplaneseatingviewer/AirplaneSeatingFrame.java
+++ AirplaneSeatingViewer/src/airplaneseatingviewer/AirplaneSeatingFrame.java
@@ -9,6 +9,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JComboBox;
 
 /**
  *
@@ -24,6 +31,17 @@ public class AirplaneSeatingFrame extends JFrame {
     private static final int FRAME_WIDTH = 300;
     private static final int FRAME_HEIGHT = 100;
     
+    // new vars
+    private static final int DEFAULT_CLASS_PASSENGERS = 0;
+    private static final int FIRST_CLASS_PASSENGERS = 2;
+    private static final int ECONOMY_CLASS_PASSENGERS = 3;
+    private JRadioButton firstClass;
+    private JRadioButton economyClass;
+    private JRadioButton windowSeat;
+    private JRadioButton centerSeat;
+    private JRadioButton aisleSeat;
+    private JComboBox passengerCombo;
+    
     public AirplaneSeatingFrame() {
         // NOTE: helper method to create components
         createComponents();
@@ -31,15 +49,40 @@ public class AirplaneSeatingFrame extends JFrame {
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
     }
     
-    // TODO create an inner class that updates 
-    // the label when find seats is clicked
+    // TODO finish the createClassChoice method that creates radio buttons for 
+    // economy and first class and returns the panel with the button group for
+    // these radio buttons attached
+    public JPanel createClassChoice() {
+        JPanel jp = new JPanel();
+        return jp;
+    }
+    
+    // TODO finish the createSeatingChoice() method that creates 3 radio buttons
+    // for window, center, aisle and returns the panel with the button group for
+    // these radio buttons attached
+    public JPanel createSeatingChoice() {
+        JPanel jp = new JPanel();
+        return jp;
+    }
+    
+    // TODO finish the createPassengerSelection(int numPass) method that takes
+    // a numPass parameter to create a combo box to allow for 1 thru numPass 
+    // passengers traveling together
+    public JPanel createPassengerSelection(int numPass) {
+        JPanel jp = new JPanel();
+        return jp;
+    }
+
+    class FindSeatListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            label.setText("searching for seats");
+        }
+    }
     
     private void createComponents() {
         button = new JButton("Find Seats");
-        // TODO create an instance of the listener (innner) class
-        
-        // TODO add the listener class to the source
-        
+        ActionListener listener = new FindSeatListener();
+        button.addActionListener(listener);
         label = new JLabel("Airplane Seating");
         JPanel panel = new JPanel();
         panel.add(button);
